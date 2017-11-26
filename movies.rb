fb45e8f9bb074ff8c1f199da97db9178da1c26ed
Alexey <gvozdmeister@gmail.com>
2017-11-26 18:40:58 +0300

    second task

--- movies.rb
+++ movies.rb
@@ -1,12 +1,17 @@
-BAD = ['Titanic', 'The Passage', 'Interstellar', 'The Pianist']
-GOOD = ['Matrix', 'The Shawshank Redemption', 'Forrest Gump', 'Leon']
+filename = "movies.txt"
 
-for arg in ARGV
-  if GOOD.include?(arg)
-    puts "#{arg} is a good movie"
-  elsif BAD.include?(arg)
-    puts "#{arg} is a bad movie"
-  else
-    puts "Haven't seen #{arg} yet"
-  end
+if ARGV.size == 1
+  filename = ARGV[0]
+elsif ARGV.size > 1
+  puts "Введите название файла в кавычках"
+  return
 end
+
+if File.exist?(filename)
+  File.open(filename, "r").each do |file|
+    puts '*' * (( file.split('|')[7].to_f - 8 ) * 10).round(0) unless file.split('|')[1].scan("Max").empty?
+  end
+else
+  puts "Файл #{filename} не найден"
+end
+

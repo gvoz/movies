8cbfcbdbdb0543baa923b5870edb74e657b1b4c2
Alexey <gvozdmeister@gmail.com>
2017-11-27 01:09:14 +0300

    fix

--- movies.rb
+++ movies.rb
@@ -2,7 +2,7 @@ require 'csv'
 require 'ostruct'
 
 filename = "movies.txt"
-keys = %i[link name year county date genres duration rating director]
+keys = %i[link name year county date genres duration rating director stars]
 
 def puts_movies(array)
   array.each{ |a| puts "#{a.name} (#{a.date}; #{a.genres}) - #{a.duration}" }
@@ -20,21 +20,20 @@ unless File.exist?(filename)
   return
 end
 
-movies = CSV.read(filename, { :col_sep => '|' }).map{ |row| OpenStruct.new(keys.zip(row).to_h) }
+movies = CSV.read(filename, col_sep: '|', headers: keys).map{ |row| OpenStruct.new(row.to_h) }
 
 puts "5 самых длинных фильмов:"
-puts_movies(movies.sort_by{ |movie| movie.duration.to_i}.first(5))
+puts_movies(movies.sort_by{ |movie| movie.duration.to_i}.reverse.first(5))
 
 puts "10 комедий (первые по дате выхода):"
-puts_movies(movies.select{ |movie| movie.genres.include?('Comedy') }.sort_by{ |movie| movie.date.to_i}.first(10) )
+puts_movies(movies.select{ |movie| movie.genres.include?('Comedy') }.sort_by(&:date).first(10) )
 
 puts "список всех режиссёров по алфавиту"
-movies.map{ |movie| movie.director }.uniq.sort_by{ |a| a.split.last }.each{ |a| puts a }
+movies.map(&:director).uniq.sort_by{ |a| a.split.last }.each{ |a| puts a }
 
 puts "количество фильмов, снятых не в США:"
 puts movies.count{ |movie| movie.county != 'USA' }
 
 puts "Статистика по месяцам:"
-dates = movies.map{ |movie| Date.strptime(movie.date, '%Y-%m').month if movie.date.size > 4 }.compact
-dates.uniq.sort.map{|month| [Date::MONTHNAMES[month], dates.count(month)]}.each{ |a| puts "#{a[0]}: #{a[1]}"}
+movies.reject{ |movie| movie.date.size < 6}.map{ |movie| Date.strptime(movie.date, '%Y-%m').month }.sort.group_by{ |e| e }.each{ |k, v| puts "#{Date::MONTHNAMES[k]}: #{v.size}"}
 
